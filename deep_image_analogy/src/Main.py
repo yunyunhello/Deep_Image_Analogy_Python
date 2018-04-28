35d6571fde7dbbd40f26d2d35efd257b79dab500
yunyunhello <675803882@qq.com>
2018-04-28 21:10:05 +0800

    Add Main.py
    for test

---
+++ deep_image_analogy/src/Main.py
@@ -0,0 +1,39 @@
+import DeepAnalygy
+import sys
+
+
+dp=DeepAnalygy
+
+if(len(sys.argv)!=9):
+	model="models/"
+	
+	A="demo/content.png"
+	BP="demo/style.png"
+	output="demo/output/"
+	
+	dp.SetModel(model)
+	dp.SetA(A)
+	dp.SetBPrime(BP)
+	dp.SetOutputDir(output)
+	dp.SetGPU(0)
+	dp.SetRatio(0.5)
+	dp.SetBlendWeight(2)
+	dp.UsePhotoTransfer(False)
+	dp.LoadInputs()
+	#dp.ComputeAnn()
+
+	else:
+		dp.SetModel(sys.argv[1])
+		dp.SetA(argv[2])
+		dp.SetBPrime(argv[3])
+		dp.SetOutputDir(argv[4])
+		dp.SetGPU(atoi(argv[5]))
+		dp.SetRatio(atof(argv[6]))
+		dp.SetBlendWeight(atoi(argv[7]))
+		if (atoi(argv[8]) == 1):
+			dp.UsePhotoTransfer(True)
+		else:
+			dp.UsePhotoTransfer(False)
+		dp.LoadInputs()
+		#dp.ComputeAnn()
+
