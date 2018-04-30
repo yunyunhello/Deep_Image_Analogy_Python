65075effb7851d2d570c2a44183d53a9b415e5f7
yunyunhello <675803882@qq.com>
2018-04-30 21:42:53 +0800

    update code

--- deep_image_analogy/src/Classifier.py
+++ deep_image_analogy/src/Classifier.py
@@ -2,10 +2,13 @@
 import numpy as np
 import caffe
 import glog as log
+import Structure  
 
 class Classifier:
 	def __init__(self,model_file,trained_file):
 		
+		self.input_geometry_=Structure.Size()
+		self.num_channels_=0
 		#???
 		#caffe.set_device(0)
 		caffe.set_mode_gpu()
@@ -17,7 +20,19 @@ class Classifier:
 		print net_.blobs['data'].data.shape 
 		log.check_eq(len(net_.inputs),1,"Network should have exactly one input.")
 		log.check_eq(len(net_.outputs),1,"Network should have exactly one output.")
-		log.check(net_.blobs['data'].channels==3 or net_.blobs['data'].channels==1, "Input layer should have 1 or 3 channels.")
+		self.num_channels_=net_.blobs['data'].shape[1]
+		log.check(self.num_channels_==3 or self.num_channels_==1, "Input layer should have 1 or 3 channels.")
 		#???
 		self.mean_=[103.939, 116.779, 123.68]
 		#print type(self.mean_)
+
+		/* Load the mean file in binaryproto format. */
+		
+	def Predict(self, img, layers, data_s, data_d, size):
+		input_geometry_.width=img.shape[1]
+		input_geometry_.height=img.shape[0]
+		
+		input_layer=net_.blobs['data']
+		input_layer.reshape(1,self.num_channels_,input_geometry_.height,input_geometry_.width)
+
+			
---
+++ deep_image_analogy/src/Structure.py
@@ -0,0 +1,5 @@
+
+class Size:
+	def __init__(self):
+		self.width=0
+		self=height=0
